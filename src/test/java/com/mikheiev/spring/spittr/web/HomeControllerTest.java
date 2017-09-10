c0a00af952d5016558da51c5b7a270cf42cd209c
Oleg Mikheiev <oleg.miky@gmail.com>
2017-09-10 22:25:48 +0200

    Added home page and HomeController

---
+++ src/test/java/com/mikheiev/spring/spittr/web/HomeControllerTest.java
@@ -0,0 +1,13 @@
+package com.mikheiev.spring.spittr.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HomeControllerTest {
+    @Test
+    public void testHomePage() {
+        HomeController controller = new HomeController();
+        assertEquals("home", controller.home());
+    }
+}
